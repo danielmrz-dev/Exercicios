2d0804c2e2e13512f9cb4be631da642a803895a7
Daniel Moraes Mariz <137360936+danielmrz-dev@users.noreply.github.com>
2023-10-27 21:14:05 -0300

    Js

---
+++ JavaScript/Ex004 - Condições/Ex001.js
@@ -0,0 +1,8 @@
+var velocidade = 80
+console.log(`A velocidade do seu carro é de ${velocidade}km/h.`)
+
+if (velocidade > 70) { // Exercício de condição simples (Apenas com o if)
+    console.log('Você está indo muito rápido, pequeno gafanhoto!')
+} 
+
+console.log('Sempre use o cinto de segurança!')
---
+++ JavaScript/Ex004 - Condições/Ex002.js
@@ -0,0 +1,8 @@
+var país = 'Brasil'
+console.log(`Nascido em ${país}.`)
+
+if (país == 'Brasil') { // Exercício de condição composta (com if e else)
+    console.log('Você é brasileiro!')
+} else {
+    console.log('Você é estrangeiro!')
+}
