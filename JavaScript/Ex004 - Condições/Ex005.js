46c9145289fd91e5b84b52d8f8131c020f92d12e
Daniel Moraes Mariz <137360936+danielmrz-dev@users.noreply.github.com>
2023-10-28 18:28:45 -0300

    conditionals and switch

---
+++ JavaScript/Ex004 - Condições/Ex005.js
@@ -0,0 +1,21 @@
+var idade = 'Azul'
+
+if (idade == 'Verde') {
+    console.log('A cor é verde.')
+} else if (idade == 'Vermelho') {
+    console.log('A cor é vermelho.')
+}   else {
+    console.log('A cor não é verde e nem vermelho.')
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ JavaScript/Ex004 - Condições/Ex006.js
@@ -0,0 +1,29 @@
+var idade = 65
+
+console.log(`Você tem ${idade} anos de idade.`)
+
+if (idade < 16) {
+    console.log('Você ainda não tem idade suficiente para votar.')
+} else if (idade <= 18 || idade >= 65) {
+    console.log('Você pode votar, mas não é obrigado.')
+} else {
+    console.log('Você é obrigado a votar.')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
