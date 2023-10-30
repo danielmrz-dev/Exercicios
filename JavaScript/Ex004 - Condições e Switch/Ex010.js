8a38ceea5bd17e3fb4c3706241b66351ac3d73a5
Daniel Moraes Mariz <137360936+danielmrz-dev@users.noreply.github.com>
2023-10-30 17:25:04 -0300

    Horas e minutos

--- JavaScript/Ex004 - Condições e Switch/Ex010.js
+++ JavaScript/Ex004 - Condições e Switch/Ex010.js
@@ -1,5 +1,6 @@
 var agora = new Date()
 hora = agora.getHours()
+minuto = agora.getMinutes()
 var texto = document.querySelector('h2.texto')
 var principal = document.querySelector('section.principal')
 var img = document.querySelector('img.img')
@@ -7,17 +8,17 @@ var titulo = document.querySelector('h1.title')
 var github = document.querySelector('div.github')
 
 if (hora < 12) {
-    texto.innerHTML = `Bom dia! Agora são ${hora} horas.`
+    texto.innerHTML = `Bom dia! Agora são ${hora}:${minuto}.`
     document.body.style.background = '#F28322'
     principal.style.background = '#F2B56B'
     img.src = 'imagens/manha.jpg'    
 }   else if (hora < 18) {
-        texto.innerHTML = `Boa tarde! Agora são ${hora} horas.`
+        texto.innerHTML = `Boa tarde! Agora são ${hora}:${minuto}.`
         principal.style.background = '#A4B3BF'
         document.body.style.background = '#659DBF'
         img.src = 'imagens/tarde.jpg'
 }       else {
-            texto.innerHTML = `Boa noite! Agora são ${hora} horas.`
+            texto.innerHTML = `Boa noite! Agora são ${hora}:${minuto}.`
             texto.style.color = 'white'                    
             principal.style.background = '#022859'
             document.body.style.background = '#022873'
