d2b583e091fc0f1094bec6a51a021cf2c0460fb4
Daniel Moraes Mariz <137360936+danielmrz-dev@users.noreply.github.com>
2023-10-31 17:09:57 -0300

    Estruturas de repetição

---
+++ JavaScript/Ex004 - Condições e Switch/Ex0012.js
@@ -0,0 +1,18 @@
+let contador = 1
+
+while (contador <= 6) { // Estrutura de repetição com teste lógico no início
+    console.log(`Numero ${contador}`)
+    contador++
+}
+
+do { // Estrutura de repetição com teste lógico no final
+    console.log(`Número ${contador}.`)
+    contador++
+} while (contador <= 34)
+
+
+
+
+
+
+
