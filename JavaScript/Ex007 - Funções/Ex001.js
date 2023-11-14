53708e2a004815d08726a5746be2d22124b7bb6a
Daniel Moraes Mariz <137360936+danielmrz-dev@users.noreply.github.com>
2023-11-14 19:47:11 -0300

    Funções

---
+++ JavaScript/Ex007 - Funções/Ex001.js
@@ -0,0 +1,10 @@
+function parimpar(n) {
+    if (n%2 == 0) {
+        return 'Este número é Par!'
+    } else {
+        return 'Este número é Ímpar!'        
+    }
+}
+
+let res = parimpar(7)
+console.log(res)
