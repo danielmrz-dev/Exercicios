2f80cf7237255db4ef1878bd74c593616cc9f321
Daniel Moraes Mariz <137360936+danielmrz-dev@users.noreply.github.com>
2023-11-14 15:14:13 -0300

    Vetores

--- JavaScript/Ex006 - Variáveis Compostas/Ex001.js
+++ JavaScript/Ex006 - Variáveis Compostas/Ex001.js
@@ -1,3 +1,12 @@
-let num = [2, 3, 5, 67, 5, 657, 567, 567, 454, 5345]
+let num = [7, 1, 4]
 
-console.log(num)
+num[3] = 2
+
+num.push(0)
+
+console.log(`O conteúdo da variável composta é ${num}`)
+console.log(`O comprimento deste vetor é ${num.length}.`)
+
+num.sort()
+
+console.log(`Em ordem crescente, os conteúdos ficam ${num}.`)
