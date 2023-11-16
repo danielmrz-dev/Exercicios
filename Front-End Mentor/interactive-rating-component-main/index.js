5fb3392a3db1a3942991aefd8fed1373d8de7d42
Daniel Moraes Mariz <137360936+danielmrz-dev@users.noreply.github.com>
2023-11-16 13:03:17 -0300

    Trying

---
+++ Front-End Mentor/interactive-rating-component-main/index.js
@@ -0,0 +1,92 @@
+let nota1 = document.getElementById('nota1')
+let nota2 = document.getElementById('nota2')
+let nota3 = document.getElementById('nota3')
+let nota4 = document.getElementById('nota4')
+let nota5 = document.getElementById('nota5')
+
+
+function clicar1() {
+    nota1.style.color = 'white'
+    nota1.style.background = '#7d8899'    
+
+    nota2.style.background = ''
+    nota2.style.color = ''  
+
+    nota3.style.background = ''
+    nota3.style.color = ''
+
+    nota4.style.background = ''
+    nota4.style.color = ''
+
+    nota5.style.background = ''
+    nota5.style.color = ''
+}
+
+function clicar2() {
+    nota1.style.background = ''
+    nota1.style.color = ''  
+
+    nota2.style.background = '#7d8899'
+    nota2.style.color = 'white'
+    
+    nota3.style.background = ''
+    nota3.style.color = ''
+
+    nota4.style.background = ''
+    nota4.style.color = ''
+
+    nota5.style.background = ''
+    nota5.style.color = ''
+}
+
+function clicar3() {    
+    nota1.style.background = ''
+    nota1.style.color = ''
+    
+    nota2.style.background = ''
+    nota2.style.color = ''
+
+    nota3.style.background = '#7d8899'
+    nota3.style.color = 'white'
+
+    nota4.style.background = ''
+    nota4.style.color = ''
+
+    nota5.style.background = ''
+    nota5.style.color = ''
+}
+
+function clicar4() {
+    nota1.style.background = ''
+    nota1.style.color = ''
+    
+    nota2.style.background = ''
+    nota2.style.color = ''
+
+    nota3.style.background = ''
+    nota3.style.color = ''
+    
+    nota4.style.background = '#7d8899'
+    nota4.style.color = 'white'    
+
+    nota5.style.background = ''
+    nota5.style.color = ''    
+}
+
+function clicar5() {
+    nota1.style.background = ''
+    nota1.style.color = ''
+    
+    nota2.style.background = ''
+    nota2.style.color = ''
+
+    nota3.style.background = ''
+    nota3.style.color = ''
+
+    nota4.style.background = ''
+    nota4.style.color = ''
+
+    nota5.style.background = '#7d8899'
+    nota5.style.color = 'white'    
+}
+
