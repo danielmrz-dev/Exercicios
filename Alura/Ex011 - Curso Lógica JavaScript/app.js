d8a1ef9f79f9ab51c7fe46857001732f5df07c7b
Daniel Moraes Mariz <137360936+danielmrz-dev@users.noreply.github.com>
2023-12-08 13:07:39 -0300

    Mais JavaScript

---
+++ Alura/Ex011 - Curso Lógica JavaScript/app.js
@@ -0,0 +1,12 @@
+alert('Bem vindo ao jogo do número secreto! 😊');
+
+let numeroSecreto = 29;
+
+let chute = prompt('Por favor, escolha um número entre 1 e 100:');
+
+if (chute == numeroSecreto) {
+    alert('Você ganhou!')
+} else {
+    alert('Você perdeu!')
+    
+}
