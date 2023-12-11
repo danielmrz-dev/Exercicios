7c53def14f32bc18702628ba01daf0a2ba4524b9
Daniel Moraes Mariz <137360936+danielmrz-dev@users.noreply.github.com>
2023-12-11 00:27:18 -0300

    salvar

---
+++ Front-End Mentor/faq-accordion/index.js
@@ -0,0 +1,10 @@
+const answer1 = document.getElementById('answer1')
+const answer2 = document.getElementById('answer2')
+const answer3 = document.getElementById('answer3')
+const answer4 = document.getElementById('answer4')
+const answer5 = document.getElementById('answer5')
+
+
+
+
+
