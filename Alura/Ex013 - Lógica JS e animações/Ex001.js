81f0ebbc15ba5db37bcab61233081e858f8e1195
Daniel Moraes Mariz <137360936+danielmrz-dev@users.noreply.github.com>
2024-01-02 18:49:27 -0300

    Js desenhos

---
+++ Alura/Ex013 - Lógica JS e animações/Ex001.js
@@ -0,0 +1,23 @@
+const tela = document.querySelector('canvas');
+const pincel = tela.getContext('2d');
+
+pincel.fillStyle = 'Lightgrey';
+pincel.fillRect(0, 0, 600, 400);
+
+pincel.fillStyle = 'Green';
+pincel.fillRect(0, 0, 200, 400);
+
+pincel.fillStyle = 'Red';
+pincel.fillRect(400, 0, 400, 400);
+
+pincel.fillStyle = 'Yellow';
+pincel.beginPath();
+pincel.moveTo(300, 200);
+pincel.lineTo(200, 400);
+pincel.lineTo(400, 400);
+pincel.fill();
+
+pincel.fillStyle = 'Blue';
+pincel.beginPath();
+pincel.arc(300, 200, 50, 0, 2 * 3.14);
+pincel.fill();
---
+++ Alura/Ex013 - Lógica JS e animações/Ex002.js
@@ -0,0 +1,61 @@
+const tela = document.querySelector('canvas');
+const pincel = tela.getContext('2d');
+
+pincel.fillStyle = 'Green';
+pincel.fillRect(0, 0, 350, 350);
+
+pincel.fillStyle = 'Black';
+pincel.fillRect(50, 50 , 80, 80);
+
+pincel.fillStyle = 'Black';
+pincel.fillRect(200, 50 , 80, 80);
+
+pincel.fillStyle = 'Black';
+pincel.fillRect(127, 130 , 75, 75);
+
+pincel.fillStyle = 'Black';
+pincel.fillRect(78, 180, 50, 120);
+
+pincel.fillStyle = 'Black';
+pincel.fillRect(200, 180, 50, 120);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// pincel.fillStyle = 'Green';
+// pincel.fillRect(0, 0, 200, 400);
+
+// pincel.fillStyle = 'Red';
+// pincel.fillRect(400, 0, 400, 400);
+
+// pincel.fillStyle = 'Yellow';
+// pincel.beginPath();
+// pincel.moveTo(300, 200);
+// pincel.lineTo(200, 400);
+// pincel.lineTo(400, 400);
+// pincel.fill();
+
+// pincel.fillStyle = 'Blue';
+// pincel.beginPath();
+// pincel.arc(300, 200, 50, 0, 2 * 3.14);
+// pincel.fill();
