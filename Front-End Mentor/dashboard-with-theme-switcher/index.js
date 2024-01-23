79eb53caf46f146d00eb34d3c68f966cc36510d9
Daniel Moraes Mariz <137360936+danielmrz-dev@users.noreply.github.com>
2024-01-23 20:59:04 -0300

    :o

---
+++ Front-End Mentor/dashboard-with-theme-switcher/index.js
@@ -0,0 +1,9 @@
+const header = document.querySelector('header');
+const body = document.querySelector('body');
+const headerFollowers = document.querySelector('.header__followers');
+
+const darkMode = document.querySelector('.checkbox');
+
+darkMode.addEventListener('change', () => {
+    
+})
