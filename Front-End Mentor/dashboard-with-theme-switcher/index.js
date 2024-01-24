7ebdb421fc45285df4fcbbafe4fe3d3508f2131b
Daniel Moraes Mariz <137360936+danielmrz-dev@users.noreply.github.com>
2024-01-24 18:02:11 -0300

    finished

--- Front-End Mentor/dashboard-with-theme-switcher/index.js
+++ Front-End Mentor/dashboard-with-theme-switcher/index.js
@@ -1,14 +1,17 @@
 const header = document.querySelector('header');
 const body = document.querySelector('body');
 const followersContainers = document.querySelectorAll('.followers__containers');
+const thirdFollowersContainer = document.querySelector('.followers__bg');
 const overviewContainers = document.querySelectorAll('.overview__containers');
+const overviewTitle = document.querySelector('.overview__title');
 
 const darkMode = document.querySelector('.checkbox');
-
 
 darkMode.addEventListener('change', () => {
     header.classList.toggle('active-bg');
     body.classList.toggle('active-bg');
+    overviewTitle.classList.toggle('active-texts');
+    thirdFollowersContainer.classList.toggle('active-bg-two');
     
     followersContainers.forEach(element => {
         element.classList.toggle('active-bg-two');
@@ -17,5 +20,5 @@ darkMode.addEventListener('change', () => {
     overviewContainers.forEach(element => {
         element.classList.toggle('active-bg-two');
     });    
-
 })
+
