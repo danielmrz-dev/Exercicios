cacf8f1bf840e3bb4f3c3175bbe1b085bbdf2b0c
Daniel Moraes Mariz <137360936+danielmrz-dev@users.noreply.github.com>
2024-01-23 22:08:24 -0300

    por hoje é só

--- Front-End Mentor/dashboard-with-theme-switcher/index.js
+++ Front-End Mentor/dashboard-with-theme-switcher/index.js
@@ -1,9 +1,21 @@
 const header = document.querySelector('header');
 const body = document.querySelector('body');
-const headerFollowers = document.querySelector('.header__followers');
+const followersContainers = document.querySelectorAll('.followers__containers');
+const overviewContainers = document.querySelectorAll('.overview__containers');
 
 const darkMode = document.querySelector('.checkbox');
 
+
 darkMode.addEventListener('change', () => {
+    header.classList.toggle('active-bg');
+    body.classList.toggle('active-bg');
     
+    followersContainers.forEach(element => {
+        element.classList.toggle('active-bg-two');
+    });
+
+    overviewContainers.forEach(element => {
+        element.classList.toggle('active-bg-two');
+    });    
+
 })
