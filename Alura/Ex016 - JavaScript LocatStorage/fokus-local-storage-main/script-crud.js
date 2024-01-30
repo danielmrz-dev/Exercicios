396db88b2a52472b2b5b7ee7c8e8102b05a9b4e0
Daniel Moraes Mariz <137360936+danielmrz-dev@users.noreply.github.com>
2024-01-30 20:14:36 -0300

    :f

--- Alura/Ex016 - JavaScript LocatStorage/fokus-local-storage-main/script-crud.js
+++ Alura/Ex016 - JavaScript LocatStorage/fokus-local-storage-main/script-crud.js
@@ -6,6 +6,10 @@ const ulTarefas = document.querySelector('.app__section-task-list');
 
 const tarefas =  JSON.parse(localStorage.getItem('tarefas')) || [];
 
+function atualizarTarefas() {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+}
+
 function criarElementoTarefa(tarefa) {
     const li = document.createElement('li');
     li.classList.add('app__section-task-list-item');
@@ -26,6 +30,20 @@ function criarElementoTarefa(tarefa) {
     const botao = document.createElement('button');
     botao.classList.add('app_button-edit');
     const imagemBotao = document.createElement('img');
+
+    botao.onclick = () => {
+        const novaDescricao = prompt('Qual é o novo nome da tarefa?');
+
+        if (novaDescricao === null || novaDescricao.length == 0) {
+            
+        } else {
+            
+        }
+
+        paragrafo.textContent = novaDescricao
+        tarefa.descricao = novaDescricao
+        atualizarTarefas();
+    }
 
     imagemBotao.setAttribute('src', '/imagens/edit.png');
 
@@ -51,7 +69,7 @@ formAdicionarTarefa.addEventListener('submit', (evento) => {
     tarefas.push(tarefa);
     const elementoTarefa = criarElementoTarefa(tarefa);
     ulTarefas.append(elementoTarefa);
-    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+    atualizarTarefas();
 
     textarea.value = '';
     formAdicionarTarefa.classList.add('hidden')
