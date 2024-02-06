fb8f7d5fbb79e1960977bd1337218685699025c4
Daniel Moraes Mariz <137360936+danielmrz-dev@users.noreply.github.com>
2024-02-06 18:54:17 -0300

    que diacho de projeto

--- Front-End Mentor/todo-app/index.js
+++ Front-End Mentor/todo-app/index.js
@@ -1,58 +1,109 @@
 const checkbox = document.querySelector("#todo-checkbox");
-const checkboxLabel = document.querySelector(
-    ".main__todo-list-item-description"
-);
+const checkboxLabel = document.querySelector(".main__todo-list-item-label");
 const addTaskBtn = document.querySelector(".main__add-new-task");
 const TaskInput = document.querySelector(".main__input");
 const tasksContainer = document.querySelector(".main__todo-list");
 const lastDiv = document.querySelector(".main__todo-left-n-clear");
 
-const listItem = document.querySelector("main__todo-list-item");
-const deleteTask = document.querySelector(".main__icon-cross");
+const listItem = document.querySelector(".main__todo-list-item");
+const deleteTaskBtn = document.querySelector(".main__icon-cross");
+
+let tasks = JSON.parse(localStorage.getItem('tarefas')) || [];
 
 TaskInput.focus();
 
-checkbox.addEventListener("change", () => {
-    checkboxLabel.classList.toggle("line-through");
+function updateTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function loadTasks() {
+    const storedTasks = localStorage.getItem("tasks");
+    if (storedTasks) {
+        tasks = JSON.parse(storedTasks);
+        tasks.forEach(task => {
+            addTaskElement(task);
+        });
+    }
+}
+
+window.addEventListener("load", () => {
+    loadTasks();
 });
 
 addTaskBtn.addEventListener("click", () => {
     if (TaskInput.value.length == 0) {
         alert("Please write a valid task!");
+        TaskInput.focus();
     } else {
-        const tasks = [];
         const task = {
             description: TaskInput.value,
         };
         tasks.push(task);
-        localStorage.setItem("tasks", JSON.stringify(tasks));
-        addTaskElement();
+        addTaskElement(task)
+        updateTasks();
+        TaskInput.innerHTML = '';
         TaskInput.focus();
     }
 });
 
-function addTaskElement() {
+tasks.forEach(task => {
+    const taskElement = addTaskElement(task)    
+    newTaskLabel.append(taskElement);
+    updateTasks();
+    TaskInput.focus();
+});
+
+function addTaskElement(task) {
     const newTaskContainer = document.createElement("div");
     newTaskContainer.classList.add("main__todo-list-item", "new-container");
 
     const newTaskCheckbox = document.createElement("input");
     newTaskCheckbox.type = "checkbox";
-    newTaskCheckbox.classList.add("main__todo-list-checkbox");
+    newTaskCheckbox.classList.add("main__todo-list-checkbox", "-order-1");
 
-    const newTaskDescription = document.createElement("label");
+    const newTaskLabel = document.createElement("label");
+    newTaskLabel.classList.add("main__todo-list-item-label", "new-label");
+
+    const newTaskDescription = document.createElement("p");
     newTaskDescription.classList.add(
-        "main__todo-list-item-description",
-        "new-description"
+        "new-description",
+        "main__todo-list-item-description"
     );
-    newTaskDescription.innerHTML = TaskInput.value;
+    newTaskDescription.textContent = task.description;
 
     const newTaskCrossBtn = document.createElement("img");
     newTaskCrossBtn.classList.add("main__icon-cross", "cursor-pointer");
     newTaskCrossBtn.setAttribute("src", "images/icon-cross.svg");
 
     lastDiv.parentNode.insertBefore(newTaskContainer, lastDiv);
-    newTaskContainer.append(newTaskCheckbox);
-    newTaskContainer.append(newTaskDescription);
+
+    newTaskLabel.append(newTaskCheckbox);
+    newTaskLabel.append(newTaskDescription);
+    newTaskContainer.append(newTaskLabel);
     newTaskContainer.append(newTaskCrossBtn);
+
     TaskInput.value = "";
+    TaskInput.focus();
+    return newTaskLabel;
 }
+
+const toggleModeBtn = document.querySelector('.main__mode-icon');
+const darkModeCheckbox = document.querySelector('#sun-moon');
+const title = document.querySelector('.main__title');
+const background = document.querySelector('body');
+
+darkModeCheckbox.addEventListener('change', () => {
+    if (darkModeCheckbox.checked) {
+        toggleModeBtn.setAttribute('src', 'images/icon-sun.svg')
+        background.classList.add('dark-bg');
+        TaskInput.classList.add('grey-container')
+        lastDiv.classList.add('grey-container')
+    } else {
+        toggleModeBtn.setAttribute('src', 'images/icon-moon.svg')
+        background.classList.remove('dark-bg');
+        TaskInput.classList.remove('grey-container')
+        lastDiv.classList.remove('grey-container')
+    }    
+})
+
+
