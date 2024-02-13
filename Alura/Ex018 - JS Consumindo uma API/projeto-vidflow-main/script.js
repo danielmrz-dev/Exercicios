1775b52d9d846c3df4fd684a16fccd63115df179
Daniel Moraes Mariz <137360936+danielmrz-dev@users.noreply.github.com>
2024-02-13 19:39:00 -0300

    very good

--- Alura/Ex018 - JS Consumindo uma API/projeto-vidflow-main/script.js
+++ Alura/Ex018 - JS Consumindo uma API/projeto-vidflow-main/script.js
@@ -6,7 +6,7 @@ async function searchAndShowVideos() {
         const videos = await search.json();
         videos.forEach((video) => {
             if (video.categoria == "") {
-                throw new Error('Vídeo sem categoria!')
+                throw new Error("Vídeo sem categoria!");
             }
             containerVideos.innerHTML += `
                 <li class="videos__item">
@@ -17,6 +17,7 @@ async function searchAndShowVideos() {
                             ${video.titulo}
                         </h3>
                         <p class="titulo-canal">${video.descricao}</p>
+                        <p class="categoria" hidden>${video.categoria}</p>
                     </div>
                 </li>
             `;
@@ -27,3 +28,48 @@ async function searchAndShowVideos() {
 }
 
 searchAndShowVideos();
+
+const barraDePesquisa = document.querySelector(".pesquisar__input");
+
+barraDePesquisa.addEventListener("input", filtrarPesquisa);
+
+function filtrarPesquisa() {
+    const videos = document.querySelectorAll(".videos__item");
+    if (barraDePesquisa.value != "") {
+        for (let video of videos) {
+            let titulo = video
+                .querySelector(".titulo-video")
+                .textContent.toLowerCase();
+            let valorFiltro = barraDePesquisa.value.toLowerCase();
+
+            if (!titulo.includes(valorFiltro)) {
+                video.style.display = "none";
+            } else {
+                video.style.display = "block";
+            }
+        }
+    } else {
+        video.style.display = "block";
+    }
+}
+
+const botaoCategoria = document.querySelectorAll(".superior__item");
+
+botaoCategoria.forEach((botao) => {
+    let nomeCategoria = botao.getAttribute("name");
+    botao.addEventListener("click", () => filtrarPorCategoria(nomeCategoria));
+});
+
+function filtrarPorCategoria(filtro) {
+    const videos = document.querySelectorAll(".videos__item");
+    for (let video of videos) {
+        let categoria = video.querySelector(".categoria").textContent.toLowerCase();
+        let valorFiltro = filtro.toLowerCase();
+
+        if (!categoria.includes(valorFiltro) && valorFiltro != "tudo") {
+            video.style.display = 'none';
+        } else {
+            video.style.display = 'block';
+        }
+    }
+}
