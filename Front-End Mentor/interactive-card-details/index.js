f032fd670c6422d0e0d2a0077b218f2404c0faf8
Daniel Moraes Mariz <137360936+danielmrz-dev@users.noreply.github.com>
2024-02-15 21:24:57 -0300

    sae

---
+++ Front-End Mentor/interactive-card-details/index.js
@@ -0,0 +1,12 @@
+const numberOnCard = document.querySelector('.cards__front-number');
+const nameOnCard = document.querySelector('.cards__name');
+const expDateOnCard = document.querySelector('.cards__exp-date');
+const cvcOnCard = document.querySelector('.cards__back-number');
+const nameInput = document.querySelector('#cardholder-name');
+const cardNumberInput = document.querySelector('#card-number');
+const monthInput = document.querySelector('#exp-date-month');
+const yearInput = document.querySelector('#exp-date-year');
+const cvcInput = document.querySelector('#cvv');
+const confirmBtn = document.querySelector('.cards__button');
+
+nameInput.focus();
