f724883c3cc516f6f2f4c46c23ef6686c281bf21
Daniel Moraes Mariz <137360936+danielmrz-dev@users.noreply.github.com>
2024-02-18 14:53:09 -0300

    chart js

--- Front-End Mentor/Para fazer/expenses-chart-component-main/index.js
+++ Front-End Mentor/Para fazer/expenses-chart-component-main/index.js
@@ -1,20 +1,57 @@
-const ctx = document.querySelector('.chart__chart');
+const ctx = document.querySelector(".chart__chart");
+
+const plugin = {
+    id: "customCanvasBackgroundColor",
+    beforeDraw: (chart, args, options) => {
+        const { ctx } = chart;
+        ctx.save();
+        ctx.globalCompositeOperation = "destination-over";
+        ctx.fillStyle = options.color || "#99ffff";
+        ctx.fillRect(0, 0, chart.width, chart.height);
+        ctx.restore();
+    },
+};
+
+let labelsX = [];
 
 new Chart(ctx, {
-    type: 'bar',
+
+    type: "bar",
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            borderWidth: 1
-        }]
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange", "Teste"],
+        datasets: [
+            {
+                label: "",
+                data: [12, 1, 3, 5, 2, 3, 7],
+                borderWidth: 0,
+                borderRadius: 3,
+                borderSkipped: false,
+                backgroundColor: '#ec775f',
+            },
+        ],
     },
     options: {
+        plugins: {
+            customCanvasBackgroundColor: {
+                color: "transparent",
+            },
+            legend: false,
+        },
         scales: {
-        y: {
-            beginAtZero: true
-        }
-        }
-    }
+            y: {
+                display: false,
+                beginAtZero: true,
+                grid: {
+                    display: false,
+                },
+            },
+            x: {
+                display: false,
+                grid: {
+                    display: false,
+                },
+            },
+        },
+    },
+    plugins: [plugin],
 });
