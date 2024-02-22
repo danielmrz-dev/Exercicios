24c06ff68b0dbb99b2ed5c56138adafa60c45864
Daniel Moraes Mariz <137360936+danielmrz-dev@users.noreply.github.com>
2024-02-22 20:55:57 -0300

    sort

--- Alura/Ex019 - JS - Métodos de Array/alura_books-projeto_inicial/app/metodoFilter.js
+++ Alura/Ex019 - JS - Métodos de Array/alura_books-projeto_inicial/app/metodoFilter.js
@@ -1,10 +1,10 @@
-const botoes = document.querySelectorAll('.btn')
-botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
+const botoes = document.querySelectorAll(".btn");
+botoes.forEach((btn) => btn.addEventListener("click", filtrarLivros));
 
 function filtrarLivros() {
-    const elementoBtn = document.getElementById(this.id)
-    const categoria = elementoBtn.value
-    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria)
-    exibirOsLivrosNaTela(livrosFiltrados)
-    console.log(categoria)
+    const elementoBtn = document.getElementById(this.id);
+    const categoria = elementoBtn.value;
+    let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria);
+    exibirOsLivrosNaTela(livrosFiltrados);
+    console.log(categoria);
 }
