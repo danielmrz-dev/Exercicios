73a5920e1483d3f7663e4d7ca68e630d7d829a2d
Daniel Moraes Mariz <137360936+danielmrz-dev@users.noreply.github.com>
2024-03-01 12:31:23 -0300

    :S

--- Alura/Ex020 - JS - Criando Requisições/aluraplay-requisicoes-main/js/buscarVideo.js
+++ Alura/Ex020 - JS - Criando Requisições/aluraplay-requisicoes-main/js/buscarVideo.js
@@ -9,20 +9,17 @@ async function buscarVideo(evento) {
     const lista = document.querySelector("[data-lista]");
 
     while (lista.firstChild) {
-        lista.removeChild(lista.firstChild)
+        lista.removeChild(lista.firstChild);
     }
 
-    busca.forEach((element) =>
-        lista.appendChild(
-            constroiCard(
-                element.titulo,
-                element.descricao,
-                element.url,
-                element.imagem
-            )
-        )
-    );
+    busca.forEach(elemento => lista.appendChild(
+        constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
+
+    if (busca.length == 0) {
+        lista.innerHTML = `<h2 class="mensagem__titulo">Não existem vídeos com esse termo</h2>`
+    }
 }
 
 const botaoDePesquisa = document.querySelector("[data-botao-pesquisa]");
-botaoDePesquisa.addEventListener("click", (evento) => buscarVideo(evento));
+
+botaoDePesquisa.addEventListener("click", evento => buscarVideo(evento))
