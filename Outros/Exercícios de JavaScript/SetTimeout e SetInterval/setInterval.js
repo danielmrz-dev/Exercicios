496a3b81b40dbdecc30600a925029095b0b4bfdb
Daniel Moraes Mariz <137360936+danielmrz-dev@users.noreply.github.com>
2024-03-04 14:03:19 -0300

    timer xD

--- Outros/Exercícios de JavaScript/SetTimeout e SetInterval/setInterval.js
+++ Outros/Exercícios de JavaScript/SetTimeout e SetInterval/setInterval.js
@@ -1,12 +1,20 @@
+// let contador = 0;
 
-let contador = 0;
+// let intervalo = setInterval(() => {
+//     console.log(contador)
+//     if (contador >= 10) {
+//         clearInterval(intervalo)
+//     }
+//     contador++
+// }, 1000);
+
+const timer = document.querySelector(".timer");
+
+timer.innerHTML = 60;
 
 let intervalo = setInterval(() => {
-    console.log(contador)
-    if (contador >= 10) {
-        clearInterval(intervalo)
+    timer.innerHTML -= 1;
+    if (timer.innerHTML <= 0) {
+        clearInterval(intervalo);
     }
-    contador++
-}, 1000);
-
-
+}, 200);
