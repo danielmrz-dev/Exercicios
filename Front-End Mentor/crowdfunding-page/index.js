5c2e188f831fbb3a7ce40c4ec7933678bd7fae6f
Daniel Moraes Mariz <137360936+danielmrz-dev@users.noreply.github.com>
2024-03-05 19:10:49 -0300

    finished design

---
+++ Front-End Mentor/crowdfunding-page/index.js
@@ -0,0 +1,11 @@
+const menuIcon = document.querySelector('.header__menu-icon')
+
+menuIcon.addEventListener('click', () => {
+    let imgSrc = menuIcon.getAttribute('src')
+    
+    // if (imgSrc === 'images/icon-hamburger.svg') {
+    //     menuIcon.setAttribute('src', 'images/icon-close-menu.svg')
+    // } else {
+    //     menuIcon.setAttribute('src', 'images/icon-hamburger.svg')
+    // }
+})
