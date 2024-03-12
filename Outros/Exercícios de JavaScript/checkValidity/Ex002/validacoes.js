161f15ebf04740cd09f782cef4a8ffa0394cd277
Daniel Moraes Mariz <137360936+danielmrz-dev@users.noreply.github.com>
2024-03-12 20:16:36 -0300

    ye

---
+++ Outros/Exercícios de JavaScript/checkValidity/Ex002/validacoes.js
@@ -0,0 +1,32 @@
+const titulo = document.querySelector(".titulo")
+const input = document.querySelector("#nome")
+const mensagemDeErro = document.querySelector(".mensagem-de-erro")
+
+const mensagens = {
+    nome: {
+        tooShort: "Digite mais caracteres",
+        tooLong: "Digite menos caracteres",
+        valueMissing: "Digite algo",
+    },
+}
+
+const tiposDeErro = [
+    "tooShort",
+    "tooLong",
+    "valueMissing"
+]
+
+input.addEventListener("blur", () => validaNome())
+input.addEventListener("invalid", (evento) => evento.preventDefault())
+
+function validaNome() {
+    let mensagem = "";
+    tiposDeErro.forEach((erro) => {
+        if (input.validity[erro]) {
+            mensagem = mensagens[input.name][erro];            
+            mensagemDeErro.innerText = mensagem
+        }
+    })    
+}
+
+console.log({} === {});
