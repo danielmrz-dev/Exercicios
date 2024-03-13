0b163e3d3d1922767ef015fc9b05297e1569f58e
Daniel Moraes Mariz <137360936+danielmrz-dev@users.noreply.github.com>
2024-03-13 14:04:09 -0300

    validações e mais validações

--- Outros/Exercícios de JavaScript/checkValidity/Ex002/validacoes.js
+++ Outros/Exercícios de JavaScript/checkValidity/Ex002/validacoes.js
@@ -1,7 +1,4 @@
-import validaNome from "./valida-nome.js"
-
-const input = document.querySelectorAll("input")
-const mensagemDeErro = document.querySelector(".mensagem-de-erro")
+const inputs = document.querySelectorAll("input")
 const submitBtn = document.querySelector("button")
 
 const mensagens = {
@@ -10,32 +7,45 @@ const mensagens = {
         tooLong: "Nome muito longo!",
         valueMissing: "O campo nome não pode ficar vazio!",
     },
+    rg: {
+        tooShort: "RG curto!",
+        tooLong: "RG longo!",
+        valueMissing: "O campo RG não pode ficar vazio!",
+    },
+    cpf: {
+        tooShort: "CPF curto!",
+        tooLong: "CPF longo!",
+        valueMissing: "O campo CPF não pode ficar vazio!",
+    },
 }
 
 const tiposDeErro = [
+    "valueMissing",
+    "typeMismatch",
+    "patternMismatch",
     "tooShort",
-    "tooLong",
-    "valueMissing"
-]
+    "customError",
+];
 
+inputs.forEach((campo) => {
+    campo.addEventListener("blur", () => validaCampo(campo));
+    campo.addEventListener("invalid", (evento) => evento.preventDefault());
+})
 
+function validaCampo(campo) {
+    let mensagem = "";
+    tiposDeErro.forEach((erro) => {
+        if (campo.validity[erro]) {
+            mensagem = mensagens[campo.name][erro];
+        }
+    })
 
-// input.addEventListener("blur", () => validaNome())
-// input.addEventListener("invalid", (evento) => evento.preventDefault())
-
-// function validaNome() {
-//     let mensagem = "";
-//     tiposDeErro.forEach((erro) => {
-//         if (input.validity[erro]) {
-//             mensagem = mensagens[input.name][erro];            
-//         }
-//     })
+    const mensagemErro = campo.parentNode.querySelector(".mensagem-de-erro");
+    const validadorDeInput = campo.checkValidity();
     
-//     const validadorDeInput = input.checkValidity();
-
-//     if (!validadorDeInput) {
-//         mensagemDeErro.innerText = mensagem        
-//     } else {
-//         mensagemDeErro.innerText = "";        
-//     }
-// }
+    if (!validadorDeInput) {
+        mensagemErro.innerText = `${mensagem}`
+    } else {
+        mensagemErro.innerText = "";
+    }
+}
