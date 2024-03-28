d00b8f9047fb75bf228f9b8ddc3565d8a772623f
Daniel Moraes Mariz <137360936+danielmrz-dev@users.noreply.github.com>
2024-03-28 12:31:46 -0300

    finished

--- Alura/Ex023 - JS - BackEnd - NodeJs/funcoes.js
+++ Alura/Ex023 - JS - BackEnd - NodeJs/funcoes.js
@@ -1,8 +1,39 @@
 // parâmetros ou argumentos
 // retorno
 
-function exibeNomeEstudante(nome) {
-    console.log(nome);
+function exibeInfosEstudante(nome, nota) {
+    console.log(`O nome é ${nome} e sua nota foi ${nota}.`);
 }
 
-exibeNomeEstudante("Daniel");
+// exibeInfosEstudante("Ana", 10);
+
+// ------------------------------------------------------------
+
+function exibeEstudante(nome, nota) {
+    return `O nome é ${nome} e sua nota foi ${nota}.`;
+}
+
+// console.log(exibeEstudante("Daniel", 10))
+
+// ------------------------------------------------------------
+
+function somaNumeros(a, b) {
+    return a + b;
+}
+
+const resultado = somaNumeros(5, 9);
+
+// console.log(resultado);
+
+// ------------------------------------------------------------
+
+function ehPalindromo(palavra) {
+    if (palavra === palavra.split("").reverse().join("")) {
+        return `A palavra ${palavra} é um palíndromo!`;
+    } else {
+        return `A palavra ${palavra} não é um palíndromo!`;
+    }
+}
+
+console.log(ehPalindromo("reviver"));
+console.log(ehPalindromo("Natal"));
