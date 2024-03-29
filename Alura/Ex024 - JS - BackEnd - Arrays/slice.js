2754aa7da622ae4f81bdc36d91682c863906c911
Daniel Moraes Mariz <137360936+danielmrz-dev@users.noreply.github.com>
2024-03-29 19:51:57 -0300

    review

--- Alura/Ex024 - JS - BackEnd - Arrays/slice.js
+++ Alura/Ex024 - JS - BackEnd - Arrays/slice.js
@@ -3,6 +3,15 @@ const listaEstudantes = ["Daniel", "Ana", "Gercino", "Flor", "Eni", "Leo", "Duda
 const salaUm = listaEstudantes.slice(0, listaEstudantes.length / 2)
 const salaDois = listaEstudantes.slice(listaEstudantes.length / 2)
 
-console.log(salaUm);
-console.log(salaDois);
+// console.log(salaUm);
+// console.log(salaDois);
+
+const lista = ["Aluno1", "Aluno2", "Aluno3", "Aluno4"]
+
+const sala1 = lista.slice(0, lista.length / 2)
+const sala2 = lista.slice(lista.length / 2)
+
+console.log(sala1);
+console.log(sala2);
+
 
