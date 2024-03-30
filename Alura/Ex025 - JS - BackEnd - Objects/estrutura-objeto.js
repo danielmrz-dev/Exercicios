ebe9e20257add858282304db43c20d520e6d388f
Daniel Moraes Mariz <137360936+danielmrz-dev@users.noreply.github.com>
2024-03-30 19:03:29 -0300

    objetos

---
+++ Alura/Ex025 - JS - BackEnd - Objects/estrutura-objeto.js
@@ -0,0 +1,10 @@
+const aluno = {
+    nome: "Daniel Moraes",
+    cpf: "40014305844",
+    idade: 33,
+    turma: "Javascript"
+}
+
+console.log(`O nome do aluno é ${aluno.nome}.`);
+console.log(`Os três primeiros digitos do CPF deste aluno são ${aluno.cpf.substring(0, 3)}.`);
+
---
+++ Alura/Ex025 - JS - BackEnd - Objects/objeto.js
@@ -0,0 +1,10 @@
+const listaCPFs = ['131123131321', '321313213213', '64643121684'];
+
+const infosPessoa = {
+    nome: "José",
+    cpf: "136546236136",
+    idade: 35
+}
+
+infosPessoa.nome = "Daniel";
+console.log(infosPessoa);
