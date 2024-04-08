3e091442809cd68c355fd0caf52251242c116396
Daniel Moraes Mariz <137360936+danielmrz-dev@users.noreply.github.com>
2024-04-08 14:31:39 -0300

    responsiveness

--- Front-End Mentor/insure-landing-page/src/script.js
+++ Front-End Mentor/insure-landing-page/src/script.js
@@ -1,5 +1,6 @@
 const menuIcon = document.querySelector(".menu-icon")
 const checkbox = document.querySelector(".checkbox")
+const menuUl = document.querySelector(".menu")
 
 checkbox.addEventListener("change", () => {
     if (checkbox.checked) {
@@ -7,4 +8,12 @@ checkbox.addEventListener("change", () => {
     } else {
         menuIcon.setAttribute("src", "../images/icon-hamburger.svg")        
     }
+})
+
+menuUl.addEventListener("click", (e) => {
+    let classes = e.target.className;
+    if (checkbox.checked && !(classes.includes("menu-item") || classes.includes("menu-link"))) {
+        checkbox.checked = false
+        menuIcon.setAttribute("src", "../images/icon-hamburger.svg")  
+    }    
 })
