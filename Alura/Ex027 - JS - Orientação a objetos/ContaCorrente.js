a5f8a94bc2a5f6d1873c42f6b8e74780d4f15c18
Daniel Moraes Mariz <137360936+danielmrz-dev@users.noreply.github.com>
2024-05-03 18:43:10 -0300

    constructor

--- Alura/Ex027 - JS - Orientação a objetos/ContaCorrente.js
+++ Alura/Ex027 - JS - Orientação a objetos/ContaCorrente.js
@@ -1,7 +1,25 @@
+import { Cliente } from "./Cliente.js";
+
 export class contaCorrente {
+    static numeroDeContas = 0;
     agencia;
-    // #saldo = 0;
+    _cliente;
     _saldo = 0;
+    // #saldo = 0;
+
+    set cliente(novoValor) {
+        if (novoValor instanceof Cliente) {
+            this._cliente = novoValor;             
+        }
+    }
+
+    get cliente() {
+        return this._cliente
+    }
+
+    get saldo() {
+        return this._saldo
+    }
 
     sacar(valor) {
         if (this._saldo >= valor) {
@@ -13,5 +31,16 @@ export class contaCorrente {
     depositar(valor) {
         if (valor <= 0) return // Early return
         this._saldo += valor;
+    }
+
+    transferir(valor, conta) {
+        const valorSacado = this.sacar(valor)
+        conta.depositar(valorSacado)
+    }
+
+    constructor (agencia, cliente) {
+        this.agencia = agencia;
+        this._cliente = cliente;
+        contaCorrente.numeroDeContas += 1;
     }
 }
--- Alura/Ex027 - JS - Orientação a objetos/index.js
+++ Alura/Ex027 - JS - Orientação a objetos/index.js
@@ -1,26 +1,42 @@
 import { contaCorrente } from "./ContaCorrente.js";
 import { Cliente } from "./Cliente.js";
 
-const cliente1 = new Cliente();
+const cliente1 = new Cliente("Daniel", 99999999999); // Usando o construtor
+const contaCorrenteCliente1 = new contaCorrente(1001, cliente1);
 
-cliente1.nome = "Daniel";
-cliente1.cpf = 99999999999;
+const cliente2 = new Cliente("Ana", 97699999999); // Usando o construtor
+const contaCorrenteCliente2 = new contaCorrente(1001, cliente2);
 
-const cliente2 = new Cliente();
+const cliente3 = new Cliente("Ana", 97699999999); // Usando o construtor
+const contaCorrenteCliente3 = new contaCorrente(1001, cliente3);
 
-cliente2.nome = "Ana";
-cliente2.cpf = 99999976759;
+const cliente4 = new Cliente("Ana", 97699999999); // Usando o construtor
+const contaCorrenteCliente4 = new contaCorrente(1001, cliente4);
 
-const contaCorrenteDaniel = new contaCorrente();
+console.log(contaCorrenteCliente1);
+console.log(contaCorrenteCliente2);
+console.log(contaCorrenteCliente3);
+console.log(contaCorrenteCliente4);
+console.log(`Atualmente, o ByteBank possui ${contaCorrente.numeroDeContas} contas ativas.`);
 
-contaCorrenteDaniel.agencia = 1001;
-// contaCorrenteDaniel.#saldo = 200000000;
 
-contaCorrenteDaniel.depositar(100);
-contaCorrenteDaniel.depositar(100);
-contaCorrenteDaniel.depositar(100);
-const valorSacado = contaCorrenteDaniel.sacar(50);
+// const conta2 = new contaCorrente()
+// conta2.cliente = cliente1// conta2.agencia = 120;
 
-console.log(valorSacado);
+// console.log(conta2.cliente);
+// console.log(conta2.saldo);
+// cliente1.cpf = 1
 
-console.log(contaCorrenteDaniel._saldo);
+
+// contaCorrenteAna.agencia = 1001;
+// contaCorrenteAna.cliente = cliente2
+
+// contaCorrenteDaniel.depositar(500)
+// console.log(contaCorrenteDaniel);
+
+// let valor = 150
+// contaCorrenteDaniel.transferir(valor, contaCorrenteAna)
+// console.log(contaCorrenteDaniel);
+// console.log(contaCorrenteAna);
+
+
