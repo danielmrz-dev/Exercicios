27208dceb809d8789ae93fe63d7df6828e14cd9f
Daniel Moraes Mariz <137360936+danielmrz-dev@users.noreply.github.com>
2024-05-05 15:47:13 -0300

    herança

--- Alura/Ex028 - JS - POO - Herança, Poliformismo/js-heranca-polimorfismo-projetoinicial/Conta.js
+++ Alura/Ex028 - JS - POO - Herança, Poliformismo/js-heranca-polimorfismo-projetoinicial/Conta.js
@@ -1,26 +1,53 @@
+import { Cliente } from "./Cliente.js";
+
 export class Conta {
     constructor(saldoInicial, cliente, agencia){
         this._saldo = saldoInicial;
-        this.cliente = cliente;
-        this.agencia = agencia;
+        this._cliente = cliente;
+        this._agencia = agencia;
+    }
+
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente(){
+        return this._cliente;
+    }
+
+    get saldo(){
+        return this._saldo;
     }
 
     sacar(valor){
-        taxa = valor * 1.1;
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
+        let taxa = 1;
+        if (this._tipo == "corrente") {
+            taxa = 1.1;
+        }
+
+        if (this._tipo == "salario") {
+            taxa = 1.05;
+        }
+
+        if (this._tipo == "empresarial") {
+            taxa = 1.15;
+        }
+
+        const valorSacado = taxa * valor
+        if(this._saldo >= valorSacado){
+            this._saldo -= valorSacado;
+            return valorSacado;
         }
     }
 
     depositar(valor){
-        if(valor <= 0) return; 
         this._saldo += valor;           
     }
 
     tranferir(valor, conta){        
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
-        
     }
 }
--- Alura/Ex028 - JS - POO - Herança, Poliformismo/js-heranca-polimorfismo-projetoinicial/index.js
+++ Alura/Ex028 - JS - POO - Herança, Poliformismo/js-heranca-polimorfismo-projetoinicial/index.js
@@ -5,11 +5,12 @@ import { Conta } from "./Conta.js";
 
 const cliente1 = new Cliente("Ricardo", 11122233309);
 
-const contaCorrenteRicardo = new Conta(0, cliente1, 1001);
+const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
 contaCorrenteRicardo.depositar(500);
 contaCorrenteRicardo.sacar(100);
 
-const contaPoupancaRicardo = new Conta(50, cliente1, 1001);
+const contaPoupancaRicardo = new ContaPoupanca(50, cliente1, 1001);
+contaPoupancaRicardo.sacar(10)
 
 console.log(contaCorrenteRicardo);
 console.log(contaPoupancaRicardo);
