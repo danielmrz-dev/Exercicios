2d804f636a251a13487234e3f53e98b719526798
Daniel Moraes Mariz <137360936+danielmrz-dev@users.noreply.github.com>
2024-05-13 19:52:20 -0300

    encapsulamento

--- Alura/Ex029 - JS - POO - Conceitos/User.js
+++ Alura/Ex029 - JS - POO - Conceitos/User.js
@@ -1,14 +1,32 @@
 export default class User {
+
+    // ATRIBUTOS PRIVADOS
+    #nome // Declarar fora do contrutor pra poder utilizar
+    #email
+    #nascimento
+    #role
+    #ativo
+
     constructor(nome, email, nascimento, role, ativo = true) {
-        this.nome = nome;
-        this.email = email;
-        this.nascimento = nascimento;
-        this.role = role || "estudante";
-        this.ativo = ativo
+        this.#nome = nome;
+        this.#email = email;
+        this.#nascimento = nascimento;
+        this.#role = role || "estudante";
+        this.#ativo = ativo
+    }
+
+    #montaObjetoUser() {
+        return ({
+            nome: this.#nome,
+            email: this.#email,
+            nascimento: this.#nascimento,
+            role: this.#role,
+            ativo: this.#ativo
+        })
     }
 
     exibirInfos() {
-        return `${this.nome}, ${this.email}`
+        const objetoUser = this.#montaObjetoUser()
+        return objetoUser
     }
-
 }
--- Alura/Ex029 - JS - POO - Conceitos/index.js
+++ Alura/Ex029 - JS - POO - Conceitos/index.js
@@ -3,4 +3,9 @@ import Admin from "./Admin.js";
 import Docente from "./Docente.js";
 
 const novoUser = new Admin("Ana", "ana@ana.com", "20/09/1990")
-console.log(novoUser);
+// console.log(novoUser);
+// console.log(novoUser.exibirInfos());
+
+const novoUser2 = new Docente("Daniel", "dan@dan", "25/03/1994")
+console.log(novoUser2.exibirInfos);
+
