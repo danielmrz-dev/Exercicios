612ac3ed1941eb42e43517386145628a43b3efb4
Daniel Moraes Mariz <137360936+danielmrz-dev@users.noreply.github.com>
2024-05-25 18:14:55 -0300

    trying to create the tag filter

--- Front-End Mentor/Para fazer/static-job-listings-vite/src/js/script.js
+++ Front-End Mentor/Para fazer/static-job-listings-vite/src/js/script.js
@@ -1,21 +1,20 @@
 import axios from "axios";
 
 const filter = document.querySelector(".header__filter");
-// const filterContainer = document.querySelector(".header__flter-items-container");
+const filterContainer = document.querySelector(".header__filter-items-container");
 const jobListContainer = document.querySelector(".job-list");
 const clearFilterBtn = document.querySelector("[data-clear]");
-const removeTagBtn = document.querySelectorAll(".header__filter-item-X");
-const tags = document.querySelectorAll(".job-list__tag");
+// const removeTagBtn = document.querySelectorAll(".header__filter-item-X");
+// const tags = document.querySelectorAll(".job-list__tag");
 
 const API = "data.json";
+const data = await axios.get(API);
+const jobs = await data.data;
 
-showJobs();
+showJobs(jobs);
 
-async function showJobs() {
-    const data = await axios.get(API);
-    const jobs = await data.data;
-    
-    jobs.forEach((job) => {
+async function showJobs(listOfJobs) {
+    listOfJobs.forEach((job) => {
         jobListContainer.innerHTML += `
         <li class="job-list__item ${job.featured ? "border-left" : ""}">        
             <img src="${job.logo}" alt="" class="job-list__item-logo">
@@ -38,23 +37,67 @@ async function showJobs() {
             </div>
             <hr>
 
-            <div class="job-list__tags">
-                
+            <div class="job-list__tags">                
                 <button class="job-list__tag">${job.role}</button>            
                 <button class="job-list__tag">${job.level}</button>            
-                ${job.languages.map(language => `<button class="job-list__tag">${language}</button>`).join("")}            
-                ${job.tools.map(tool => `<button class="job-list__tag">${tool}</button>`).join("")}
+                ${job.languages.map((language) =>`<button class="job-list__tag">${language}</button>`).join("")}            
+                ${job.tools.map((tool) => `<button class="job-list__tag">${tool}</button>`).join("")}
             </div>
 
         </li>
-        `
-    })
+        `;
+    });
 }
 
+let tagList = [];
+let allFilteredJobs = [];
+
+function showFilteredJobs() {
+    let filteredJobs = jobs.filter((job) => {
+        tagList.includes(job.role) &&
+        tagList.includes(job.level)
+    });
+
+    jobListContainer.innerHTML = "";
+    console.log(filteredJobs);
+    showJobs(allFilteredJobs[0]);
+}
 
 document.addEventListener("click", (e) => {
-    if (e.target.className.includes(("job-list__tag"))) {
+    if (e.target.matches(".job-list__tag")) {
         filter.style.display = "grid";
-        console.log(e.target.innerText);
+        const tagName = e.target.innerText;
+        if (!tagList.includes(tagName)) {
+            tagList.push(tagName);
+            addTagFilter();
+            showFilteredJobs();
+
+            // Here goes the filter logic to show on the screen only the jobs containing the clicked tags
+            
+        }
     }
-})
+});
+
+function addTagFilter() {
+    filterContainer.innerHTML = "";
+    tagList.forEach((tag) => {
+        filterContainer.innerHTML += `
+        <li class="header__filter-items">
+            <span class="header__filter-item">${tag}</span>
+            <button class="header__filter-item-X">
+                <img src="images/icon-remove.svg" alt="">
+            </button>
+        </li>
+        `;
+    });
+}
+
+function clearFilter() {
+    tagList = [];
+    filterContainer.innerHTML = '';
+    filter.style.display = "none";
+    jobListContainer.innerHTML = '';
+    showJobs(jobs);
+}
+
+clearFilterBtn.addEventListener("click", clearFilter);
