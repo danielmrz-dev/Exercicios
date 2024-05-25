eda1de4c346db428f8c939fce88e19347572ce65
Daniel Moraes Mariz <137360936+danielmrz-dev@users.noreply.github.com>
2024-05-24 21:00:33 -0300

    working on the filters

--- Front-End Mentor/Para fazer/static-job-listings-vite/src/js/script.js
+++ Front-End Mentor/Para fazer/static-job-listings-vite/src/js/script.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 
-const filterContainer = document.querySelector(".header__flter-items-container");
+const filter = document.querySelector(".header__filter");
+// const filterContainer = document.querySelector(".header__flter-items-container");
 const jobListContainer = document.querySelector(".job-list");
 const clearFilterBtn = document.querySelector("[data-clear]");
 const removeTagBtn = document.querySelectorAll(".header__filter-item-X");
-const tag = document.querySelectorAll(".job-list__tag");
+const tags = document.querySelectorAll(".job-list__tag");
 
-const API = "data.json"
+const API = "data.json";
+
+showJobs();
 
 async function showJobs() {
     const data = await axios.get(API);
@@ -14,35 +17,44 @@ async function showJobs() {
     
     jobs.forEach((job) => {
         jobListContainer.innerHTML += `
-        <li class="job-list__item">        
-        <img src="${job.logo}" alt="" class="job-list__item-logo">
+        <li class="job-list__item ${job.featured ? "border-left" : ""}">        
+            <img src="${job.logo}" alt="" class="job-list__item-logo">
 
-        <div class="job-list__item-company-container">
-            <strong class="job-list__item-company">${job.company}</strong>
-            ${job.new ? "<span class='job-list__item-new'>New!</span>" : ""}
-            ${job.featured ? "<span class='job-list__item-featured'>Featured</span>" : ""}          
-        </div>
+            <div class="job-list__infos">
 
-        <h2 class="job-list__item-position">${job.position}</h2>
+                <div class="job-list__item-company-container">
+                    <strong class="job-list__item-company">${job.company}</strong>
+                    ${job.new ? "<span class='job-list__item-new'>New!</span>" : ""}
+                    ${job.featured ? "<span class='job-list__item-featured'>Featured</span>" : ""}          
+                </div>
 
-        <div class="job-list__item-info-container">
-            <span class="job-list__item-info">${job.postedAt}</span>
-            <span class="job-list__item-info">${job.contract}</span>
-            <span class="job-list__item-info">${job.location}</span>
-        </div>
+                <h2 class="job-list__item-position">${job.position}</h2>
 
-        <hr>
+                <div class="job-list__item-info-container">
+                    <span class="job-list__item-info">${job.postedAt}</span>
+                    <span class="job-list__item-info">${job.contract}</span>
+                    <span class="job-list__item-info">${job.location}</span>
+                </div>
+            </div>
+            <hr>
 
-        <div class="job-list__tags">
-            <button class="job-list__tag">${job.role}</button>
-            <button class="job-list__tag">${job.level}</button>
-            ${job.languages.map(language => `<button class="job-list__tag">${language}</button>`).join("")}
-            ${job.tools.map(tool => `<button class="job-list__tag">${tool}</button>`).join("")}
-        </div>
+            <div class="job-list__tags">
+                
+                <button class="job-list__tag">${job.role}</button>            
+                <button class="job-list__tag">${job.level}</button>            
+                ${job.languages.map(language => `<button class="job-list__tag">${language}</button>`).join("")}            
+                ${job.tools.map(tool => `<button class="job-list__tag">${tool}</button>`).join("")}
+            </div>
 
         </li>
         `
     })
 }
 
-showJobs();
+
+document.addEventListener("click", (e) => {
+    if (e.target.className.includes(("job-list__tag"))) {
+        filter.style.display = "grid";
+        console.log(e.target.innerText);
+    }
+})
