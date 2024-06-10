e955f60b29ce84dcd905e5381fb07770ab07ff50
Bowie <137360936+danielmrz-dev@users.noreply.github.com>
2024-06-10 15:23:57 -0300

    componentização

--- Outros/Santander Coders/React/my-app/src/App.jsx
+++ Outros/Santander Coders/React/my-app/src/App.jsx
@@ -1,20 +1,14 @@
 import React from "react";
-
+import "./styles/App.css"
+import Navbar from "./components/Navbar/Navbar";
 // Um componente em classe é uma classe que herda a classe Component do React e retorna HTML dentro do método render().
 // É como se fosse uma tag HTML personalizada para retornar o HTML e algumas funcionalidades.
 
 class App extends React.Component {
-
-  // Render é o método responsável por renderizar o HTML do componente
-  render() {
-    return (
-        <div>
-          <h1>Aprendendo React ⚛️</h1>
-  
-          <p>Isso é um parágrafo!</p>
-        </div>
-    );
-  }
+    // Render é o método responsável por renderizar o HTML do componente
+    render() {
+        return <Navbar/>;
+    }
 }
 
 export default App;
