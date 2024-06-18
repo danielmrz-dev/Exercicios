a3c408dabb154fcf8316eb9bee595edb4b2e7a83
Bowie <137360936+danielmrz-dev@users.noreply.github.com>
2024-06-18 18:32:49 -0300

    Instalando TS e configurando tsconfig.json

---
+++ Outros/Plataforma ADA/Front-end em React/Typescript/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function somar(n1, n2) {
+    return n1 + n2;
+}
+somar(3, 78);
+console.log("hello");
---
+++ Outros/Plataforma ADA/Front-end em React/Typescript/index.ts
@@ -0,0 +1,7 @@
+function somar(n1: number, n2: number) {
+    return n1 + n2;
+}
+
+somar(3, 78);
+
+console.log("hello");
