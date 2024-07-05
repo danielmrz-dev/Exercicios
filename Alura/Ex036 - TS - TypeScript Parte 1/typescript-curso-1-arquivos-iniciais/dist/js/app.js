edd055dc2c111963b44691fee52a43d9c0a2ad6e
Bowie <137360936+danielmrz-dev@users.noreply.github.com>
2024-07-05 20:03:49 -0300

    Revisando TypeScript

--- Alura/Ex036 - TS - TypeScript Parte 1/typescript-curso-1-arquivos-iniciais/dist/js/app.js
+++ Alura/Ex036 - TS - TypeScript Parte 1/typescript-curso-1-arquivos-iniciais/dist/js/app.js
@@ -1,5 +1,3 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const negociacao_js_1 = require("./models/negociacao.js");
-const negociacao = new negociacao_js_1.Negociacao(new Date(), 10, 200);
+import { Negociacao } from "./models/negociacao.js";
+const negociacao = new Negociacao(new Date(), 10, 200);
 console.log(negociacao.valor);
