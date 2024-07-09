237f4c028836617a79d05578fd0b7a9f05401880
Bowie <137360936+danielmrz-dev@users.noreply.github.com>
2024-07-09 20:53:36 -0300

    Decorators

--- Alura/Ex038 - TS - TypeScript Parte 3/typescript-curso-3/app/src/views/view.ts
+++ Alura/Ex038 - TS - TypeScript Parte 3/typescript-curso-3/app/src/views/view.ts
@@ -1,3 +1,5 @@
+import { logarTempoDeExecucao } from "../decorators/tempo-de-execucao.js";
+
 export abstract class View<T> {
 
     protected elemento: HTMLElement;
@@ -15,11 +17,12 @@ export abstract class View<T> {
         }
     }
 
+    @logarTempoDeExecucao()
     public update(model: T): void {
         let template = this.template(model);
         if (this.escapar) {
             template = template
-                .replace(/<script>[\s\S]*?<\/script>/, '');
+            .replace(/<script>[\s\S]*?<\/script>/, '');
         }
         this.elemento.innerHTML = template;
     }
