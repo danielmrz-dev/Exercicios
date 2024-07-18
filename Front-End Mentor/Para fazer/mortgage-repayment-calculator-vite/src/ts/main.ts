a791fa1a65529228be667da1d19aee1e65bf12ff
Daniel <137360936+danielmrz-dev@users.noreply.github.com>
2024-07-18 16:04:33 -0300

    Form validations

--- Front-End Mentor/Para fazer/mortgage-repayment-calculator-vite/src/ts/main.ts
+++ Front-End Mentor/Para fazer/mortgage-repayment-calculator-vite/src/ts/main.ts
@@ -1,23 +1,40 @@
-// import { calculateMortgage } from "./calculateMortgage.js";
-
+import { calculateMortgage } from "./calculateMortgage.js";
 import { validateField, validateMortgageType } from "./validations.js";
 
-const form: HTMLFormElement | null =
-    document.querySelector(".calculator__form");
-// const mortgageTypeRepayment: HTMLInputElement | null = document.querySelector("#mortgage-type-repayment");
-// const mortgageTypeInterest: HTMLInputElement | null = document.querySelector("#mortgage-type-interest");
+const mortgageAmount = document.querySelector("#mortgage-amount") as HTMLInputElement;
+const mortgageTerm = document.querySelector("#mortgage-term") as HTMLInputElement;
+const interestRate = document.querySelector("#interest-rate") as HTMLInputElement;
+const repaymentOption = document.querySelector("#mortgage-type-repayment") as HTMLInputElement;
+const interestOnlyOption = document.querySelector("#mortgage-type-interest") as HTMLInputElement;
+const form: HTMLFormElement | null = document.querySelector(".calculator__form");
+
+interestRate.addEventListener("input", function() {
+    this.value = this.value.replace(/[^0-9,]/g, '');
+});
 
 if (form) {
     form.addEventListener("submit", (evento) => {
         evento.preventDefault();
-        validateField("#mortgage-amount", ".currency-icon");
-        validateField("#mortgage-term", ".years-icon");
-        validateField("#interest-rate", ".percentage-icon");
-        validateMortgageType();
-        // const amount = Number(mortgageAmount?.value);
-        // const term = Number(mortgageTerm?.value);
-        // const interest = Number(interestRate?.value);
-        // const monthlyPayment = calculateMortgage(amount, interest, term);
-        // const interestOnly = Number((((monthlyPayment * 12) * term) - amount).toFixed(2))
+        const isMortgageAmountValid: boolean = validateField("#mortgage-amount", ".currency-icon");
+        const isMortgageTermValid: boolean = validateField("#mortgage-term", ".years-icon");
+        const isInterestRateValid: boolean = validateField("#interest-rate", ".percentage-icon");
+        const isMortgageTypeValid: boolean = validateMortgageType();
+
+        const numberMortgageAmount = parseFloat(mortgageAmount.value);
+        const numberInterestRate = parseFloat(interestRate.value.replace(",", "."));
+        const numberMortgageTerm = parseFloat(mortgageTerm.value);
+        
+        if (isMortgageAmountValid && isMortgageTermValid && isInterestRateValid && isMortgageTypeValid) {
+            let monthlyPayment =  calculateMortgage(numberMortgageAmount, numberInterestRate, numberMortgageTerm);
+            if (repaymentOption.checked) {
+                monthlyPayment = monthlyPayment
+                //= renderizar o resultado na tela
+                
+            } else if (interestOnlyOption.checked) {
+                monthlyPayment = ((monthlyPayment * 12) * Number(mortgageTerm.value)) - Number(mortgageAmount.value)                
+                //= renderizar o resultado na tela
+            }
+            console.log(monthlyPayment);           
+        }
     });
 }
