3c1591c90df46bcded8547aa37b4d91a60ecb48d
Daniel <137360936+danielmrz-dev@users.noreply.github.com>
2024-09-07 14:59:22 -0300

    Diretiva NgStyle

--- Outros/Angular/NgStyle/src/app/app.component.ts
+++ Outros/Angular/NgStyle/src/app/app.component.ts
@@ -7,4 +7,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NgStyle';
+
+  fontSize: number = 20
+  textColor: "red" | "orange" = "orange"
+  styleString: string = 'font-size: 20px; color: orange;'
+
+  stylesObj = {
+    'font-size': this.fontSize + 'px',
+    'color': this.textColor
+  }
+
+  increaseFontSize() {
+    this.fontSize += 1
+    this.styleString = `font-size: ${this.fontSize}px; color: ${this.textColor}`
+    this.stylesObj = {
+      'font-size': this.fontSize + 'px',
+      'color': this.textColor
+    }
+  }
+  
+  decreaseFontSize() {
+    this.fontSize -= 1
+    this.styleString = `font-size: ${this.fontSize}px; color: ${this.textColor}`
+    this.stylesObj = {
+      'font-size': this.fontSize + 'px',
+      'color': this.textColor
+    }
+  }
+
+  toggleFontColor() {
+    if (this.textColor === "red") {
+      this.textColor = "orange"
+    } else {
+      this.textColor = "red"
+    }
+
+    this.styleString = `font-size: ${this.fontSize}px; color: ${this.textColor}`
+    this.stylesObj = {
+      'font-size': this.fontSize + 'px',
+      'color': this.textColor
+    }
+  }
 }
