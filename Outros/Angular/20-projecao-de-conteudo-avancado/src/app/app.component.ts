539f3413dac68ad0fa183dc72706c2512a4bb97a
Daniel <137360936+danielmrz-dev@users.noreply.github.com>
2024-10-26 21:26:04 -0300

    ngtemplate com ngif

--- Outros/Angular/20-projecao-de-conteudo-avancado/src/app/app.component.ts
+++ Outros/Angular/20-projecao-de-conteudo-avancado/src/app/app.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './ng-template-outlet/ex4.html',
+  templateUrl: './ng-template-outlet/ex5.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = '20-projecao-de-conteudo-avancado';
-  condition: boolean = true
+  condition: boolean = false
 }
