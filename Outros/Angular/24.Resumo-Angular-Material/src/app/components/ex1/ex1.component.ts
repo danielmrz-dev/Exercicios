be70a7b060f6a9d9ef763669fe3bff42f77a1cb6
Daniel <137360936+danielmrz-dev@users.noreply.github.com>
2024-11-24 14:39:31 -0300

    Overview projeto

--- Outros/Angular/24.Resumo-Angular-Material/src/app/components/ex1/ex1.component.ts
+++ Outros/Angular/24.Resumo-Angular-Material/src/app/components/ex1/ex1.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
+import { CustomErrorStateMatcher } from './utils/custom-error-state-matcher';
+import { ErrorStateMatcher } from '@angular/material/core';
 
 @Component({
   selector: 'app-ex1',
   templateUrl: './ex1.component.html',
-  styleUrl: './ex1.component.scss'
+  styleUrl: './ex1.component.scss',
+  providers: [
+    {
+      provide: ErrorStateMatcher,
+      useClass: CustomErrorStateMatcher,
+    }
+  ]
 })
 export class Ex1Component {
   email: string = ""
 
+  matcher = new CustomErrorStateMatcher();
   recebeEmail(emailNovo: string) {
     this.email = emailNovo
   }
