7471c5f696e454bd0254fa5a5af7c3c067feff29
Daniel <137360936+danielmrz-dev@users.noreply.github.com>
2025-01-29 15:50:51 -0300

    Operadores RXJS

--- Outros/Angular/36.Chamadas-Http/aulas/observables/src/app/app.component.ts
+++ Outros/Angular/36.Chamadas-Http/aulas/observables/src/app/app.component.ts
@@ -1,38 +1,75 @@
 import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { ObservablesService } from './services/observables.service';
-import { firstValueFrom } from 'rxjs';
+import { catchError, map, Subscription, switchMap, take, throwError } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
+
+  subs!: Subscription;
+  subsSwitchMap!: Subscription;
   private readonly observablesService = inject(ObservablesService)
 
   ngOnInit(): void {
-    // firstValueFrom(this.observablesService.teste())
-    //   .then((resposta) => {
-    //     console.log('Observable transformado em promise', resposta);
-    //   })
-
-    // this.observablesService.teste().subscribe((valor) => {
-    //   console.log('Observable inscrito', valor);      
+    // this.observablesService.getUsers().subscribe({
+    //   next(value) {
+    //     console.log(value);
+    //   },
+    //   error(err) {
+    //     console.log(err);
+    //   },
     // })
 
-    this.observablesService.getUsers().subscribe({
-      next(value) {
-        console.log(value);
-      },
-      error(err) {
-        console.log(err);
-        
-      },
-      
-      
+    // this.subs = this.observablesService.obsInterval().subscribe((value) => {
+    //   console.log('Inscrição no componente => ', value);
+    // })
+
+    // this.observablesService.getTodoInfos(1).pipe(
+    //   map((todoResponse) => {
+    //     const newTodo = {
+    //       id: todoResponse.id,
+    //       title: todoResponse.title,
+    //     }
+    //     return newTodo;
+    //   }),
+    //   catchError((erro) => {
+    //     return throwError(() => new Error('Deu ruim', erro))
+    //   })
+    // ).subscribe((value) => {
+    //   console.group('Todo 1 => ', value);
+    // })
+
+    // this.observablesService.getTodoInfos(2).subscribe((value) => {
+    //   console.group('Todo 2 => ', value);
+    // })
+
+    this.subsSwitchMap = this.observablesService.obsOne().pipe(
+      switchMap((valorObs1) => {
+        console.log('Componente inscrição Obs1', valorObs1);
+        return this.observablesService.obsTwo()
+      }),
+      take(1)
+    ).subscribe((valueObs2) => {
+      console.log('Componente inscrição Obs2', valueObs2);
     })
   }
+
+  unsubscribeInterval() {
+    console.log('Desinscrevendo...');
+
+    this.subs?.unsubscribe();
+  }
+
+  unsubscribeSwitchMap() {
+    this.subsSwitchMap?.unsubscribe();
+  }
+
 }
--- Outros/Angular/36.Chamadas-Http/aulas/observables/src/app/services/observables.service.ts
+++ Outros/Angular/36.Chamadas-Http/aulas/observables/src/app/services/observables.service.ts
@@ -1,6 +1,13 @@
 import { HttpClient } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
-import { Observable, of } from "rxjs";
+import { catchError, map, Observable, of, throwError } from "rxjs";
+
+interface ITodoInfosResponse {
+    id: number;
+    userId: number;
+    title: string;
+    completed: boolean;
+}
 
 @Injectable({
     providedIn: 'root'
@@ -8,16 +15,75 @@ import { Observable, of } from "rxjs";
 export class ObservablesService {
 
     private readonly http = inject(HttpClient)
-
-    teste() {
-        return of('[1, 2]')
-        // return new Observable((observer) => {
-        //     observer.next('Observable')
-        //     observer.next('Observable 2')
-        // })
-    }
-
     getUsers(): Observable<any> {
         return this.http.get<any>('https://jsonplaceholder.typicode.com/users');
+    }
+
+    getTodoInfos(id: number): Observable<Partial<ITodoInfosResponse>> {
+        return this.http.get<ITodoInfosResponse>(`https://jsonplaceholder.typicode.com/todos/${id}`).pipe(
+            map((todoResponse) => {
+                const newTodo = {
+                    id: todoResponse.id,
+                    title: todoResponse.title,
+                }
+                return newTodo;
+            }),
+            catchError((erro) => {
+                return throwError(() => new Error('Deu ruim', erro))
+            })
+        );
+    }
+
+    obsInterval() {
+        return new Observable((observer) => {
+            console.log('Fora do intervalo => OBS Interval');
+            const intervalo = setInterval(() => {
+                observer.next('Valor emitido pelo observable');
+                console.log('Intervalo sendo executado');
+            }, 2000);
+
+            return () => {
+                console.log('Limpeza do observable e parando o intervalo.');
+                observer.complete();
+                clearInterval(intervalo);
+            }
+        })
+    }
+
+    obsOne() {
+        return new Observable((observer) => {
+            setTimeout(() => {
+                observer.next('OBS1 => 1')
+            }, 1000);
+            setTimeout(() => {
+                observer.next('OBS1 => 2')
+            }, 2000);
+            setTimeout(() => {
+                observer.next('OBS1 => 3')
+            }, 3000);
+            return () => {
+                console.log("Limpeza 1");                
+            }
+        })
+    }
+
+    obsTwo() {
+        return new Observable((observer) => {
+            setTimeout(() => {
+                observer.next('OBS2 => 1')
+            }, 1000);
+            
+            setTimeout(() => {
+                observer.next('OBS2 => 2')
+            }, 2000);
+            
+            setTimeout(() => {
+                observer.next('OBS2 => 3')
+            }, 3000);
+
+            return () => {
+                console.log("Limpeza 2");                
+            }
+        })
     }
 }
