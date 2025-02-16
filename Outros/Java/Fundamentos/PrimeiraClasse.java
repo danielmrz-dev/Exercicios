3c7146731b7148362a6e7a0b49081dc89ae35a82
Daniel Moraess Mariz <danielmrz.dev@gmail.com>
2025-02-16 13:05:22 -0300

    Exercicios Java

---
+++ Outros/Java/Fundamentos/PrimeiraClasse.java
@@ -0,0 +1,34 @@
+package Fundamentos;
+
+public class PrimeiraClasse {
+
+    public static void main(String[] args) {
+        // System.out.println("Chegamo galera!");
+
+        //Tipos
+        int inteiro = 10;
+        double duplo = 10.5;
+        float flutuante = 10.5f;
+        long longo = 1084613156L;
+        boolean booleano = true;
+        String texto = "10";
+
+        // if-else
+        if (inteiro < duplo) {
+            System.out.println("Duplo é maior!");
+        } else {
+            System.out.println("Inteiro é maior!");            
+        }
+
+        // While
+        while (inteiro < 20) {
+            System.out.println(inteiro);
+            inteiro++;
+        }
+
+        // For 
+        for(int numero = 10; numero < 20; numero = numero + 2) {
+            System.out.println("O valor atual é " + numero + ".");
+        }
+    }
+}
