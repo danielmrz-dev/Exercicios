d2b88ea42557440a2c972536909d4c084236641a
Daniel Moraess Mariz <danielmrz.dev@gmail.com>
2025-02-17 21:11:12 -0300

    @Component e @Autowired

--- Backend/DecolaTech-DIO/SpringBoot/primeiros-passos/src/main/java/dio/springboot/PrimeirosPassosApplication.java
+++ Backend/DecolaTech-DIO/SpringBoot/primeiros-passos/src/main/java/dio/springboot/PrimeirosPassosApplication.java
@@ -1,11 +1,12 @@
 package dio.springboot;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.w3c.dom.ls.LSOutput;
 
 @SpringBootApplication
 public class PrimeirosPassosApplication {
 	public static void main(String[] args) {
-		System.out.println("Testando");
+		SpringApplication.run(PrimeirosPassosApplication.class, args);
 	}
-
 }
