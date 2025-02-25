e98a732ecf824010a5123d90f37dd1830aea3ad5
Daniel Moraess Mariz <danielmrz.dev@gmail.com>
2025-02-25 12:43:36 -0300

    Animações adicionadas

---
+++ Alura/Ex040 - Animações com Angular/2806-animacoes-projeto-base/src/app/animations.ts
@@ -0,0 +1,36 @@
+import { trigger, state, style, transition, animate } from "@angular/animations";
+
+export const highlightedStateTrigger = trigger("highlightedState", [
+    state('default', style({})),
+    state('highlighted', style({
+        borderColor: '#b2b6ff',
+        filter: 'brightness(92%)'
+    })),
+    transition('* => *', [
+        animate(300),
+    ])
+])
+
+export const showStateTrigger = trigger("showState", [
+    transition(':enter', [
+        style({
+            opacity: 0,
+        }),
+        animate(200, style({
+            opacity: 1
+        }))
+    ]),
+    transition(':leave', [
+        animate(200, style({
+            opacity: 0
+        }))
+    ])
+])
+
+export const checkButtonTrigger = trigger('checkButton', [
+    transition('* => checked', [
+        animate('400ms ease-in', style({
+            transform: 'scale(0.4)'
+        }))
+    ])
+])
