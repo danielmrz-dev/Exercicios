64bc8a694a85fd3b9467f65d7b08ac858b9293d6
Daniel Moraess Mariz <danielmrz.dev@gmail.com>
2025-03-10 20:48:15 -0300

    modelagem dos dados

--- Outros/Java/Alura - SpringBoot/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ Outros/Java/Alura - SpringBoot/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,6 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Primeiro Projeto Spring sem Web!");
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
