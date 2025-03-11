d830a1a8eddcd7f745281afd4cf28be5a35fffd7
Daniel Moraess Mariz <danielmrz.dev@gmail.com>
2025-03-11 14:21:51 -0300

    Cria estrutura inicial

---
+++ Outros/Java/Alura - SpringBoot/TabelaFIPE/src/test/java/desafio/TabelaFIPE/TabelaFipeApplicationTests.java
@@ -0,0 +1,13 @@
+package desafio.TabelaFIPE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabelaFipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
