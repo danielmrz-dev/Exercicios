25565acc1f3c8a87f42e5b389d630868e3127e81
Daniel Moraess Mariz <danielmrz.dev@gmail.com>
2025-03-12 17:36:56 -0300

    Configura o relacionamento entre as tabelas de séries e episódios no banco de dados

--- Outros/Java/Alura - SpringBoot/java-screenmatch-com-jpa/src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ Outros/Java/Alura - SpringBoot/java-screenmatch-com-jpa/src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -26,7 +26,7 @@ public class Serie {
     private String poster;
     private String sinopse;
 
-    @Transient
+    @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Episodio> episodios = new ArrayList<>();
 
     public Serie() {}
@@ -103,7 +103,10 @@ public class Serie {
 
     public List<Episodio> getEpisodios() { return episodios; }
 
-    public void setEpisodios(List<Episodio> episodios) { this.episodios = episodios; }
+    public void setEpisodios(List<Episodio> episodios) {
+        episodios.forEach(e -> e.setSerie(this));
+        this.episodios = episodios;
+    }
 
     @Override
     public String toString() {
@@ -115,6 +118,7 @@ public class Serie {
 
                 ", atores = '" + atores + '\'' +
                 ", poster = '" + poster + '\'' +
-                ", sinopse = '" + sinopse + '\'';
+                ", sinopse = '" + sinopse + '\'' +
+                ", episódios = '" + episodios + '\'';
     }
 }
