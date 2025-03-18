ce0689f4153206b6049346d45f7fb255bac95004
Daniel Moraess Mariz <danielmrz.dev@gmail.com>
2025-03-18 14:06:41 -0300

    Cria Rota Put

--- Outros/Java/Alura - SpringBoot/api/src/main/java/med/voll/api/controller/MedicoController.java
+++ Outros/Java/Alura - SpringBoot/api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,10 +1,11 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.medico.DadosMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
+import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,5 +20,25 @@ public class MedicoController {
     @Transactional
     public void cadastrarMedico(@RequestBody @Valid DadosMedico dados) {
         medicoRepository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemMedico> listaMedicos(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        return medicoRepository.findAll(paginacao).map(DadosListagemMedico::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void editaMedico(@RequestBody @Valid DadosEditaMedico dados) {
+        var medico = medicoRepository.getReferenceById(dados.id());
+        medico.atualizarInformacoes(dados);
+
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluiMedico(@PathVariable Long id) {
+//        medicoRepository.deleteById(id); EXCLUI DE FATO DO BANCO DE DADOS
+
     }
 }
