873ea596512c5cb87a78ef23357e4a0cc24cc8a0
Daniel Moraess Mariz <danielmrz.dev@gmail.com>
2025-04-12 15:17:15 -0300

    implementa testes unitários no comopnente avaliacao-estrelas

--- Outros/Angular/37-Angular-Testes/testes-organo/src/app/componentes/avaliacao-estrelas/avaliacao-estrelas.component.spec.ts
+++ Outros/Angular/37-Angular-Testes/testes-organo/src/app/componentes/avaliacao-estrelas/avaliacao-estrelas.component.spec.ts
@@ -35,7 +35,49 @@ describe('AvaliaçãoEstrelas', () => {
         const classificacao = 3;
         component.writeValue(classificacao);
         expect(component.classificacao).toBe(3);
+    });
+
+    it('deveria chamar o método onChange quando o método classificar for chamado', () => {
+        const onChangeSpy = jest.spyOn(component, 'onChange');
+        const classificacao = 4;
+        component.classificar(classificacao);
+        expect(onChangeSpy).toHaveBeenCalled();
+    });
+
+    it('deveria chamar o método onTouched quando o método classificar for chamado', () => {
+        const onTouchedSpy = jest.spyOn(component, 'onTouched');
+        component.classificar(2);
+        expect(onTouchedSpy).toHaveBeenCalled();
+    });
+
+    it('não deveria atualizar a classificação quando a propriedade readonly for true', () => {
+        const onChangeSpy = jest.spyOn(component, 'onChange');
+        component.readOnly = true;
+        component.classificar(5);
+        expect(onChangeSpy).not.toHaveBeenCalled();
+        expect(component.classificacao).not.toBe(5);
+    });
+
+    it('deveria ignorar valores inválidos e atribuir o valor 1 à classificação', () => {
+        const valoresInvalidos = [0, 'abc', -4, undefined];
+        valoresInvalidos.forEach((valor) => {
+            component.writeValue(valor as any);
+            expect(component.classificacao).toBe(1);
+        })
 
     });
+
+    it('deveria atualizar o DOM quando a classificação muda', () => {
+        const classificacao = 3;
+        component.classificar(classificacao);
+        const estrelaPreenchida = fixture.nativeElement.querySelector('.filled');
+        expect(estrelaPreenchida).toBeTruthy();
+    });
+    
+    
+    
+    
+    
+    
     
 });
