fb2391176472a29016e1bc09a945eb6425352658
Daniel Mariz <danielmrz.dev@gmail.com>
2025-07-23 10:33:37 -0300

    Ex1

--- Outros/Angular/38-RxJS/curso-rxjs-3/src/app/app.component.ts
+++ Outros/Angular/38-RxJS/curso-rxjs-3/src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { from, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'curso-rxjs-3';
+
+  dataFrom: number = 0;
+  dataOf: number = 0;
+
+  constructor() {
+    const numbersFrom$ = from([1, 2, 3, 4, 5]);
+    const numbersOf$ = of([1, 2, 3, 4, 5]);
+
+    numbersFrom$.subscribe((numbers) => {
+      console.log('Dados emitidos pelo from() => ', numbers);
+      this.dataFrom = numbers;
+    })
+
+    numbersOf$.subscribe((numbers) => {
+      console.log('Dados emitidos pelo from() => ', numbers);
+      this.dataOf = numbers[1];
+    })
+  }
 }
