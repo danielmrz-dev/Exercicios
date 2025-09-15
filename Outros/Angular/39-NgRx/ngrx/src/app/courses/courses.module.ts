b70b0a97c1cc7401b249e2aaa394248e07347e18
Daniel Mariz <danielmrz.dev@gmail.com>
2025-09-15 18:21:34 -0300

    ngrx entities

--- Outros/Angular/39-NgRx/ngrx/src/app/courses/courses.module.ts
+++ Outros/Angular/39-NgRx/ngrx/src/app/courses/courses.module.ts
@@ -28,6 +28,8 @@ import { compareLessons, Lesson } from './model/lesson';
 import { CoursesResolver } from './courses.resolver';
 import { EffectsModule } from '@ngrx/effects';
 import { CoursesEffects } from './courses.effects';
+import { StoreModule } from '@ngrx/store';
+import { coursesReducer } from './reducers/course.reducers';
 
 
 export const coursesRoutes: Routes = [
@@ -67,7 +69,8 @@ export const coursesRoutes: Routes = [
     RouterModule.forChild(coursesRoutes),
     EffectsModule.forFeature([
       CoursesEffects
-    ])
+    ]),
+    StoreModule.forFeature("courses", coursesReducer)
   ],
   declarations: [
     HomeComponent,
