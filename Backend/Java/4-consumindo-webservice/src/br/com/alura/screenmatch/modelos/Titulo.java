a3d96a769f4b752f12464d11cca1c3d70f6e3bff
Daniel Mariz <danielmrz.dev@gmail.com>
2026-05-20 20:58:46 -0300

    Gson e Records

--- Backend/Java/4-consumindo-webservice/src/br/com/alura/screenmatch/modelos/Titulo.java
+++ Backend/Java/4-consumindo-webservice/src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -1,7 +1,11 @@
 package br.com.alura.screenmatch.modelos;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Titulo implements Comparable<Titulo> {
+    @SerializedName("Title")
     private String nome;
+    @SerializedName("Year")
     private int anoDeLancamento;
     private boolean incluidoNoPlano;
     private double somaDasAvaliacoes;
@@ -13,7 +17,13 @@ public class Titulo implements Comparable<Titulo> {
         this.anoDeLancamento = anoDeLancamento;
     }
 
-    public String getNome() {
+  public Titulo(TituloOMBD meuTituloOmdb) {
+      this.nome = meuTituloOmdb.title();
+      this.anoDeLancamento = Integer.parseInt(meuTituloOmdb.year());
+      this.duracaoEmMinutos = Integer.parseInt(meuTituloOmdb.runtime().substring(0,2));
+  }
+
+  public String getNome() {
         return nome;
     }
 
@@ -67,4 +77,13 @@ public class Titulo implements Comparable<Titulo> {
     public int compareTo(Titulo outroTitulo) {
         return this.getNome().compareTo(outroTitulo.getNome());
     }
+
+  @Override
+  public String toString() {
+    return """
+      Título: %s
+      Ano de lançamento: %d
+      Duração: %d minutos
+      """.formatted(nome, anoDeLancamento, duracaoEmMinutos);
+  }
 }
---
+++ Backend/Java/4-consumindo-webservice/src/br/com/alura/screenmatch/modelos/TituloOMBD.java
@@ -0,0 +1,3 @@
+package br.com.alura.screenmatch.modelos;
+
+public record TituloOMBD(String title, String year, String runtime) {}
