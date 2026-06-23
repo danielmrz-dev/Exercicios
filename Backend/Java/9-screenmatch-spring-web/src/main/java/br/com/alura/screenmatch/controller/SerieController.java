60a19389ff0f5a4c087e96f325f9d2b6d47bcce1
Daniel Mariz <danielmrz.dev@gmail.com>
2026-06-23 18:12:04 -0300

    mais endpoints para buscar dados no banco

--- Backend/Java/9-screenmatch-spring-web/src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ Backend/Java/9-screenmatch-spring-web/src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,14 +1,40 @@
 package br.com.alura.screenmatch.controller;
 
+import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.service.SerieService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/series")
 public class SerieController {
 
-  @GetMapping("/series")
-  public String obterSerie() {
-    return "Hello danny!";
+  @Autowired
+  private SerieService serieService;
+
+  @GetMapping
+  public List<SerieDTO> obterSeries() {
+    return serieService.obterSeries();
+  }
+
+  @GetMapping("/top5")
+  public List<SerieDTO> obterMelhores5Series() {
+    return serieService.obterTop5Series();
+  }
+
+  @GetMapping("/categorias/{categoria}")
+  public List<SerieDTO> obterSeriesPorCategoria(@PathVariable String categoria) {
+    return serieService.obterSeriesPorCategoria(categoria);
+  }
+
+  @GetMapping("/lancamentos")
+  public List<SerieDTO> obterLancamentos() {
+    return serieService.obterLancamentos();
   }
 
 }
---
+++ Backend/Java/9-screenmatch-spring-web/src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -0,0 +1,53 @@
+package br.com.alura.screenmatch.service;
+
+import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.model.Categoria;
+import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SerieService {
+
+  @Autowired
+  private SerieRepository repository;
+
+  public List<SerieDTO> obterSeries() {
+    return converteParaSerieDTO(repository.findAll());
+  }
+
+  public List<SerieDTO> obterTop5Series() {
+    return converteParaSerieDTO(repository.findTop5ByOrderByAvaliacaoDesc());
+  }
+
+  public List<SerieDTO> obterSeriesPorCategoria(String categoria) {
+    return converteParaSerieDTO(repository.findByGenero(Categoria.fromPortugues(categoria)));
+  }
+
+  public List<SerieDTO> obterLancamentos() {
+    return converteParaSerieDTO(repository.findTop5ByOrderByAvaliacaoDesc());
+  }
+
+
+
+  private List<SerieDTO> converteParaSerieDTO(List<Serie> series) {
+    return series.stream()
+      .map(s -> new SerieDTO(
+        s.getId(),
+        s.getTitulo(),
+        s.getTotalTemporadas(),
+        s.getAvaliacao(),
+        s.getGenero(),
+        s.getAtores(),
+        s.getPoster(),
+        s.getSinopse()))
+      .collect(Collectors.toList());
+  }
+
+
+
+}
