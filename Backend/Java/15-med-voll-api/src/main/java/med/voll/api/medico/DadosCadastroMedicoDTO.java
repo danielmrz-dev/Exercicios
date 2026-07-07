27640f8826cd863032ec102ba388103af44c6937
Daniel Mariz <danielmrz.dev@gmail.com>
2026-07-06 23:57:41 -0300

    configurando banco de dados

---
+++ Backend/Java/15-med-voll-api/src/main/java/med/voll/api/medico/DadosCadastroMedicoDTO.java
@@ -0,0 +1,11 @@
+package med.voll.api.medico;
+
+import med.voll.api.endereco.Endereco;
+
+public record DadosCadastroMedicoDTO(
+  String nome,
+  String email,
+  String crm,
+  Especialidade especialidade,
+  Endereco endereco
+) {}
