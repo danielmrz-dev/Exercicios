fc63190e383e4084c07b7e4bad33c40914325b55
Daniel Mariz <danielmrz.dev@gmail.com>
2026-07-27 21:28:49 -0300

    service de login

---
+++ Backend/Java/19-login-github-google/src/main/java/br/com/forum_hub/controller/LoginGithubController.java
@@ -0,0 +1,29 @@
+package br.com.forum_hub.controller;
+
+import br.com.forum_hub.domain.autenticacao.github.LoginGithubService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/login/github")
+public class LoginGithubController {
+
+	@Autowired
+	private LoginGithubService loginGithubService;
+
+	@GetMapping
+	public ResponseEntity<Void> redirecionaParaGithub() {
+		var url = loginGithubService.gerarUrl();
+		var headers = new HttpHeaders();
+		headers.setLocation(URI.create(url));
+
+		return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
+	}
+}
